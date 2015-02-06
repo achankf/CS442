d1fb8d49ff7560a69f4de5eb6af9571afea26bf0
Alfred Chan <a77chan@uwaterloo.ca>
2015-02-06 17:09:26 -0500

    progress... Q6 38/52

--- Q6.rkt
+++ Q6.rkt
@@ -12,13 +12,55 @@
 
 ;; parse: s-expression -> AST
 
+; length of (static-arg-length sx) <> length of sx implies (error "wrong number")
+(define (static-arg-length sx)
+  (cond 
+    [(not (list? sx)) #f] ; not applicable -- this may be a function name, variable, etc.
+    [else (match (first sx)
+            ['with 3]
+            ['if 4]
+            ['fun 3]
+            ['mt? 2]
+            ['zero? 2]
+            ['fst 2]
+            ['rst 2]
+            ['+ 3]
+            ['- 3]
+            ['* 3]
+            ['/ 3]
+            ['kons 3]
+            ['rec 3]
+            [_ #f] ; not applicable -- checks for application is deferred to runtime (or interp-time, if you like)
+            )]
+    ))
+
+(define (make-env-out-of-vars vars)
+  (map 
+   (lambda(x) 
+     (list (first x) (parse (second x)))) 
+   vars))
+
 (define (parse sx)
+  ; error-checking
+  (let ([len (static-arg-length sx)])
+    (cond
+      [(not len) (void)] ; don't care -- not applicable
+      [(not (equal? len (length sx))) (error "wrong number")]))
+  
+  ; at this point, everything expect for "application" should have the correct number of arguments
+  
   (match sx
-    [`(with ,vars ,exp) (closure (parse exp) vars)]
+    ; keywords
+    [`(with ,vars ,exp) (closure (parse exp) (make-env-out-of-vars vars) )]
     [`(if ,exp ,texp ,fexp) (iff (parse exp) (parse texp) (parse fexp))]
     [`(fun ,args ,bdy) 
      (cond [(not (equal? (length (remove-duplicates args)) (length args))) (error "duplicate")])
      (fun args (parse bdy))]
+    [`(rec ,bindings ,exp) (rec (make-env-out-of-vars bindings) 
+                             (void) ; not used -- we make rec-exps an environment instead
+                             (parse exp))]
+    
+    ; hardcode operators to leave rooms for custom-defined functions of arity 1 or 2
     
     ; unary operators
     [`(mt? ,exp) (un-op 'mt? (parse exp))]
@@ -33,9 +75,12 @@
     [`(/ ,exp1 ,exp2) (bin-op '/ (parse exp1) (parse exp2))]
     [`(kons ,exp1 ,exp2) (bin-op 'kons (parse exp1) (parse exp2))]
     
-    [`(rec ,exp) (un-op exp (parse exp))]
+    ; application
     [(? list?) (app (parse (first sx)) (map parse (rest sx)))]
-    [x x]))
+    
+    ; var, const
+    [x x]
+    ))
 
 ;; A substitution is a (list n v), where n is a symbol and v is a value.
 
@@ -44,7 +89,7 @@
 ;; An AST is a fun or an app or a var (symbol)
 
 (define (interp-bin-op env op exp1 exp2)
-  (printf "interp-bin-op: env:~a op:~a exp1:~a exp2:~a\n" env op exp1 exp2)
+  (printf "interp-bin-op: env:~a op:~a exp1:~a exp2:~a\n\n" env op exp1 exp2)
   (let ([eval1 (interp exp1 env)]
         [eval2 (interp exp2 env)])
     (match op
@@ -79,12 +124,13 @@
   )
 
 (define (interp ast env)
-  (printf "ast:~a env:~a\n" ast env)
+  (printf "ast:~a env:~a\n\n" ast env)
   (match ast
     ; constants
     ['mt 'mt]
     ['fls 'fls]
     ['tru 'tru]
+    [(list 'kons _ _) ast]
     [(? number?) ast]
     
     ; operators
@@ -92,11 +138,11 @@
     [(bin-op op exp1 exp2) (interp-bin-op env op exp1 exp2)]
     
     ; expressions
-    [(rec vars exps bdy) empty] ; todo
-    [(iff test texp fexp) 
+    [(rec vars exps bdy) (interp bdy (append vars env))]
+    [(iff test texp fexp)
      (if (racket-bool (interp test env)) (interp texp env) (interp fexp env))]
     [(fun v bdy) (closure ast env)]
-    [(closure cast cenv) (interp cast (append env cenv))]
+    [(closure cast cenv) (interp cast (append cenv env))]
     [(app fun-exp arg-exp)
      (match (interp fun-exp env)
        [(closure (fun param body) cl-env)
@@ -104,14 +150,51 @@
         ; since functions now have multiple parameters, so create an env for the paramaters and then append it to the closure's env
         (let ([param-env (zip param (map (lambda (exp) (interp exp env)) arg-exp))])
           (interp body (append param-env cl-env)))]
-       [_ (error "not a function")]
+       [x (error "not a function")]
        )]
-    [x (second (assoc x env))]))
+    [x (let ([found (assoc x env)])
+         (cond [(not found) (error "undefined")])
+         (match (second found)
+           [(fun _ _) (interp (second (assoc x env)) env)] ; get function from environment
+           [_ (interp (second (assoc x env)) env)]))] ; this may be a variable that can be further reduced
+    ))
 
 ;; please comment out or change the name of this function in
 ;; code submitted to Marmoset
 
 ;(define (run sexp) (closure-ast (interp (parse sexp) empty)))
+
+; let-rec
+(check-expect
+ (interp 
+  (parse
+   '(rec ([factorial (fun (n)
+                          (if (zero? n) 1 (* n (factorial (- n 1)))))])
+      (factorial 3)))
+  '())
+ 6)
+
+(check-expect
+ (interp 
+  (parse
+   '(rec ([factorial (fun (n)
+                          (if (zero? n) 1 (* n (factorial (- n 1)))))])
+      (factorial 0)))
+  '())
+ 1)
+
+(check-expect
+ (interp 
+  (parse
+   '(rec ([sum-1-to-n-helper (fun (n acc)
+                                  (if (zero? n) acc (sum-1-to-n-helper (- n 1) (+ n acc))))]
+          [sum-1-to-n (fun (n)
+                           (sum-1-to-n-helper n 0))])
+      (sum-1-to-n 10)))
+  '())
+ 55)
+
+(check-expect (interp (parse `(rec ((x (fun (z) y)) (a 1012) (y a)) (x 6446))) '()) 1012)
 
 ; binary ops
 (check-expect (parse '(+ 1 2)) (bin-op '+ 1 2))
@@ -127,6 +210,8 @@
 (check-expect (interp (parse '(+ 1 2)) '()) 3)
 (check-expect (interp (parse '(+ (* 3 9) (/ 2 (- 3 1)))) '()) 28)
 (check-expect (interp (parse '(kons 123 (kons 234 mt))) '())
+              '(kons 123 (kons 234 mt)))
+(check-expect (interp (parse '(with ((tail (kons 234 mt))) (kons 123 tail))) '())
               '(kons 123 (kons 234 mt)))
 
 ; unary ops
@@ -166,6 +251,7 @@
 (check-expect (interp (parse '((fun (a b c d e f g) (kons a (kons b (kons c (kons d (kons e (kons f (kons g mt))))))))
                                123 tru fls 5 65 7 9)) '())
               '(kons 123 (kons tru (kons fls (kons 5 (kons 65 (kons 7 (kons 9 mt))))))))
+(check-expect (interp (parse '((fun (x) x) y)) '((y 123))) 123)
 
 ; others
 (check-expect (parse '(fun (x y) x)) (fun '(x y) 'x))
@@ -176,5 +262,19 @@
 (check-expect (interp (parse '(with ((x 10) (y 20)) (with ((z 4)) (* (+ x y) z)))) '()) 120)
 (check-expect (interp (parse '(fun (x y) x)) '()) (closure (fun '(x y) 'x) '()))
 (check-expect (interp (parse '((fun (x y) (kons x y)) 123 (rst (kons 123 mt)))) '()) '(kons 123 mt))
+
+(check-expect  (interp (parse '(with ((x 1) (y 2)) (fun (z) z))) '())
+               (closure (fun '(z) 'z) '((x 1) (y 2))))
+(check-expect (interp (parse '(fun (x) x)) '()) (closure (fun '(x) 'x) '()))
+
+; errors
+
+; undefined
+(check-error (interp (parse '(with ((x 10) (y 20)) (with ((z 4)) (* (+ x y) unbound-variable)))) '()))
+
+; wrong number -- too many arguments
+(check-error (interp (parse '(with ((x 10) (y 20)) (with ((z 4)) (* (+ x y) z z z z z z z z z z)))) '()))
+; wrong number -- too few arguments
+(check-error (interp (parse '(with ((x 10) (y 20)) (with ((z 4)) (* (+ x) z)))) '()))
 
 (test)
